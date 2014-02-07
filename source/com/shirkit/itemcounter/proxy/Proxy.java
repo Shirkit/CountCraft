20bdeedd54b88889c23087c0c79db6092ceb179d
Shirkit <shirkit@gmail.com>
2014-02-07 02:00:55 -0300

    Forgot about the proxy

--- source/com/shirkit/itemcounter/proxy/Proxy.java
+++ source/com/shirkit/itemcounter/proxy/Proxy.java
@@ -1,5 +1,9 @@
 package com.shirkit.itemcounter.proxy;
 
+import com.shirkit.itemcounter.ItemCounter;
+import com.shirkit.itemcounter.integration.buildcraft.BuildCraftHandler;
+
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
@@ -8,6 +12,8 @@ public class Proxy {
 	public static Proxy proxy;
 
 	public void searchForIntegration(FMLPreInitializationEvent event) {
-
+		if (Loader.isModLoaded("BuildCraft|Transport")) {
+			ItemCounter.instance.integrations.add(new BuildCraftHandler());
+		}
 	}
 }
