27d6b33fbe826427296d2debb54dd8032068c996
Shirkit <shirkit@gmail.com>
2014-02-08 20:05:08 -0300

    Support for activation/disabling the counter (send packet to server) Added sorting by ID, name or size Added different size measures: absolute, percentage, average per seconds/minutes/hours

--- source/com/shirkit/itemcounter/gui/ContainerCounter.java
+++ source/com/shirkit/itemcounter/gui/ContainerCounter.java
@@ -2,12 +2,13 @@ package com.shirkit.itemcounter.gui;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.tileentity.TileEntity;
 
 import com.shirkit.itemcounter.logic.Counter;
 
 public class ContainerCounter extends Container {
 
-	public ContainerCounter(Counter counter) {
+	public ContainerCounter(Counter counter, TileEntity tile) {
 	}
 
 	@Override
--- source/com/shirkit/itemcounter/gui/GuiCounter.java
+++ source/com/shirkit/itemcounter/gui/GuiCounter.java
@@ -1,7 +1,10 @@
 package com.shirkit.itemcounter.gui;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 
 import net.minecraft.client.Minecraft;
@@ -11,31 +14,50 @@ import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.tileentity.TileEntity;
 
 import org.lwjgl.opengl.GL11;
 
 import com.shirkit.itemcounter.logic.Counter;
+import com.shirkit.itemcounter.network.UpdateServerPacket;
+
+import cpw.mods.fml.common.network.PacketDispatcher;
 
 public class GuiCounter extends GuiContainer {
 
-	private static final ResourceLocation TEXTURE = new ResourceLocation("textures/gui/container/generic_54.png");
 	private static RenderItem render = new RenderItem();
+
+	private enum Sorting {
+		ID, Size, Name
+	}
+
+	private enum Average {
+		None, Percentage, Second, Minute, Hour
+	}
 
 	static {
 		render.zLevel = 100f;
 	}
 
+	/** Data **/
 	private Counter counter;
-	private Button nextPage, previousPage;
+
+	/** Elements **/
+	private Button nextPage, previousPage, active, sort, average;
+
+	/** Control stuff **/
 	private int currentPage = 0;
 	private int itemsPerPage = 8;
+	private Comparator<ItemStack> comparer;
+	private static Sorting currentSort = Sorting.ID;
+	private static Average currentAverage = Average.None;
+	private TileEntity tile;
 
-	public GuiCounter(Counter counter) {
-		super(new ContainerCounter(counter));
+	public GuiCounter(Counter counter, TileEntity tileEntity) {
+		super(new ContainerCounter(counter, tileEntity));
 		this.counter = counter;
+		this.tile = tileEntity;
 		this.allowUserInput = false;
-
 	}
 
 	@Override
@@ -50,19 +72,82 @@ public class GuiCounter extends GuiContainer {
 		Minecraft mc = Minecraft.getMinecraft();
 		ScaledResolution sr = new ScaledResolution(mc.gameSettings, mc.displayWidth, mc.displayHeight);
 
-		int back = sr.getScaledWidth() / 5;
+		int left = sr.getScaledWidth() / 5;
 		int top = sr.getScaledHeight() / 7;
 		int right = sr.getScaledWidth() / 3 * 2 + 5;
 		int bottom = sr.getScaledHeight() / 8 * 7;
 
 		nextPage = new Button(1, right - 10, bottom - 20, 10, 20, ">");
-		nextPage.tooltip = "Next page";
 		buttonList.add(nextPage);
 
 		previousPage = new Button(2, right - 60, bottom - 20, 10, 20, "<");
-		previousPage.tooltip = "Previous page";
 		buttonList.add(previousPage);
 
+		active = new Button(3, left, bottom - 20, 50, 20, "Enabled");
+		buttonList.add(active);
+
+		sort = new Button(4, left + 50, bottom - 20, 30, 20, "Sort");
+		buttonList.add(sort);
+
+		average = new Button(5, left + 80, bottom - 20, 50, 20, "Average");
+		buttonList.add(average);
+
+		updateActive();
+		updateAverage();
+		updateSort();
+	}
+
+	private void updateActive() {
+		if (counter.isActive()) {
+			active.displayString = "Enabled";
+			active.tooltip = "Currently counting items";
+		} else {
+			active.displayString = "Disabled";
+			active.tooltip = "Not counting items";
+		}
+	}
+
+	private void updateAverage() {
+		switch (currentAverage) {
+		case None:
+			average.tooltip = "No average";
+			break;
+
+		case Percentage:
+			average.tooltip = "Percentage average";
+			break;
+
+		case Second:
+			average.tooltip = "Items per second average";
+			break;
+
+		case Minute:
+			average.tooltip = "Items per minute average";
+			break;
+
+		case Hour:
+			average.tooltip = "Items per hour average";
+			break;
+		}
+	}
+
+	private void updateSort() {
+		switch (currentSort) {
+		case ID:
+			sort.tooltip = "Sorting by item ID";
+			comparer = new IdComparer();
+			break;
+
+		case Name:
+			sort.tooltip = "Sorting by item Name";
+			comparer = new NameComparer();
+			break;
+
+		case Size:
+			sort.tooltip = "Sorting by item Amount";
+			comparer = new SizeComparer();
+			break;
+		}
 	}
 
 	@Override
@@ -90,6 +175,21 @@ public class GuiCounter extends GuiContainer {
 		} else if (pressed == previousPage) {
 			if (currentPage > 0)
 				currentPage--;
+		} else if (pressed == active) {
+			counter.setActive(!counter.isActive());
+			UpdateServerPacket toServer = new UpdateServerPacket(tile.xCoord, tile.yCoord, tile.zCoord, counter.isActive());
+			try {
+				PacketDispatcher.sendPacketToServer(toServer.getPacket());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			updateActive();
+		} else if (pressed == sort) {
+			currentSort = Sorting.values()[(currentSort.ordinal() + 1) % Sorting.values().length];
+			updateSort();
+		} else if (pressed == average) {
+			currentAverage = Average.values()[(currentAverage.ordinal() + 1) % Average.values().length];
+			updateAverage();
 		}
 	}
 
@@ -113,16 +213,43 @@ public class GuiCounter extends GuiContainer {
 		int maxPages = counter.size() / itemsPerPage + 1;
 
 		List<ItemStack> set = counter.entrySet();
-		Collections.sort(set, new IdComparer());
+		Collections.sort(set, comparer);
 
 		for (int i = currentPage * itemsPerPage; i < currentPage * itemsPerPage + itemsPerPage && i < set.size(); i++) {
 
 			ItemStack stack = set.get(i);
-			String size = String.valueOf(stack.stackSize);
 			int numbercolor = 255 << 16 | 255 << 8 | 170;
-
 			float floatSize = stack.stackSize;
-			if (floatSize > 1000) {
+
+			String size = null;
+
+			switch (currentAverage) {
+			case None:
+				size = String.format("%.0f", floatSize);
+				break;
+
+			case Percentage:
+				floatSize = (floatSize / counter.getTotalItems()) * 100f;
+				size = String.format("%.1f", floatSize).concat("%");
+				break;
+
+			case Second:
+				floatSize /= (counter.getTicksRun() / 20f);
+				size = String.format("%.2f", floatSize);
+				break;
+
+			case Minute:
+				floatSize /= (counter.getTicksRun() / (20f * 60f));
+				size = String.format("%.2f", floatSize);
+				break;
+
+			case Hour:
+				floatSize /= (counter.getTicksRun() / (20f * 60f * 60f));
+				size = String.format("%.2f", floatSize);
+				break;
+			}
+
+			if (currentAverage == Average.None && floatSize > 1000) {
 				String sufix = "";
 				floatSize = floatSize / 1000;
 				if (floatSize > 1000) {
@@ -163,14 +290,18 @@ public class GuiCounter extends GuiContainer {
 		drawCenteredString(mc.fontRenderer, page, distNX, bottom - 13, 16777215);
 
 		// Draw tooltip
-		/*
-		 * Iterator iterator = buttonList.iterator();
-		 * 
-		 * while (iterator.hasNext()) { Button btn = (Button) iterator.next();
-		 * if (btn.isHover()) { List<String> text = new ArrayList<String>();
-		 * text.add(btn.tooltip); drawHoveringText(text, x - 155, y - 25,
-		 * mc.fontRenderer); } }
-		 */
+
+		Iterator iterator = buttonList.iterator();
+
+		while (iterator.hasNext()) {
+			Button btn = (Button) iterator.next();
+			if (btn.isHover() && btn.tooltip != null) {
+				List<String> text = new ArrayList<String>();
+				text.add(btn.tooltip);
+				drawHoveringText(text, mouseX - 155, mouseY - 25, mc.fontRenderer);
+			}
+		}
+
 	}
 
 	private class IdComparer implements Comparator<ItemStack> {
@@ -179,6 +310,21 @@ public class GuiCounter extends GuiContainer {
 		public int compare(ItemStack o1, ItemStack o2) {
 			return o1.itemID - o2.itemID;
 		}
+	}
 
+	private class NameComparer implements Comparator<ItemStack> {
+
+		@Override
+		public int compare(ItemStack o1, ItemStack o2) {
+			return o1.getDisplayName().compareTo(o2.getDisplayName());
+		}
+	}
+
+	private class SizeComparer implements Comparator<ItemStack> {
+
+		@Override
+		public int compare(ItemStack o1, ItemStack o2) {
+			return o2.stackSize - o1.stackSize;
+		}
 	}
 }
