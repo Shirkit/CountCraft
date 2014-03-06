3d08d157e943a7e437f5bec92a2758f3163bc902
Shirkit <shirkit@gmail.com>
2014-03-06 16:07:30 -0300

    fix: usr: Actually fixed the ThermalExpansion energy buffered counter @minor

--- source/com/shirkit/countcraft/CountCraft.java
+++ source/com/shirkit/countcraft/CountCraft.java
@@ -41,7 +41,7 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid = "CountCraft", name = "CountCraft", version = "0.1", dependencies = "after:BuildCraft|Transport")
+@Mod(modid = "CountCraft", name = "CountCraft", version = "0.1", dependencies = "after:BuildCraft|Transport,ThermalExpansion")
 @NetworkMod(channels = { CountCraft.CHANNEL }, packetHandler = PacketHandler.class)
 public class CountCraft {
 
--- source/com/shirkit/countcraft/integration/te/ThermalExpansionHandler.java
+++ source/com/shirkit/countcraft/integration/te/ThermalExpansionHandler.java
@@ -1,6 +1,9 @@
 package com.shirkit.countcraft.integration.te;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 import com.shirkit.countcraft.api.integration.ICounterFinder;
 import com.shirkit.countcraft.api.integration.IGuiListener;
@@ -33,6 +36,17 @@ public class ThermalExpansionHandler implements IIntegrationHandler {
 
 	@Override
 	public void postInit(FMLPostInitializationEvent event) {
+		
+		ItemStack itemStack = GameRegistry.findItemStack("ThermalExpansion", "cellBasicFrame", 8);
+		if (itemStack == null) {
+			Item item = GameRegistry.findItem("ThermalExpansion", "cellBasicFrame");
+			if (item != null)
+				itemStack = new ItemStack(item, 8);
+		}
+		
+		if (itemStack != null)
+			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(energycell, 8),  "iii", "ycy", "ibi", 'i', Item.ingotIron, 'y', "dyeYellow", 'c', Item.comparator, 'b', itemStack));
+		
 		if (event.getSide().isClient()) {
 			BufferedRenderer energyRender = new BufferedRenderer(1.0f, 0.8f, 0.4f);
 			ClientRegistry.bindTileEntitySpecialRenderer(TileCounterEnergyCell.class, energyRender);
